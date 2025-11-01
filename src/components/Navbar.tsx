f2fbb742afe0c91a127464f4e9dffce9a0fd88cf
bharathpersonalai <bharathpersonalai@gmail.com>
2025-11-01 16:49:22 +0530

    logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
             {/* Logo + Brand in one line */}
             <div className="flex items-center space-x-2">
               <img
-                src="/images/logo.png" // 👉 your logo path
+                src="/images/logo1.png" // 👉 your logo path
                 alt="Nature Hydrovation Logo"
                 className="h-12 w-auto object-contain"
               />
